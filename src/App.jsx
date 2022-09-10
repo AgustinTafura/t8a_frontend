dca3348968e9d15e7a1a5e95c2996060884dca6d
AgustinTafura <agustintafura@hotmail.com>
2022-09-10 20:30:41 -0300

    add header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss';
+import Footer from './containers/Footer';
+import Home from './containers/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <header className="App-header">
+      <div className='container'>
+        <div>
+          <tt className='tag'>
+            WEBINAR
+          </tt>
+        </div>
+        <h1>
+          El reto de humanicar el CX financiero en 2021.
+        </h1>
+        <h2>
+          <i>
+            La experiencia de un Unicorniode latam
+          </i>
+        </h2>
+        <h3>
+          Miércoles 16 de diciembre | 17 hs (Horario de Quito)
+        </h3>
+      </div>
+    </header>
+  )
+}
+
+export default Home
