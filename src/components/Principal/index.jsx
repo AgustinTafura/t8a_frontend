08c7eeecdb5b483b3eda22b1a48e9d776b9bc450
AgustinTafura <agustintafura@hotmail.com>
2022-09-11 12:38:36 -0300

    main content updated

--- src/components/Principal/index.jsx
+++ src/components/Principal/index.jsx
@@ -1,42 +1,65 @@
 import Brands from '../Brands';
+import PersonalCard from '../PersonalCard';
 import './style.scss';
 
 
 const Principal = () => {
     return (
-        <main>
-            <Brands/>
-            <section>
-                <div>
-                    <p>
-                        Te invitan este webinar, donde trataremos la temática de cómo humanizar 
-                        la experiencia del cliente de Banca y Seguros en el nuevo entorno digital.
-                    </p>
-                    <p>
-                        Además podremos conocer las estrategias que aplicó LOREM para generar una 
-                        experiencia memorable para sus clientes, mientras se convertía en el gran 
-                        Unicornio de LATAM.
-                    </p>
-                    <p>
-                        Escucha de primera mano la voz de nuestros especialistas:
-                    </p>
-                </div>
+        <main >
+            <div className='container'>
+                <div className='row'>
+                    <section className='left'>
+                        <Brands/>
+                        <div className='text-content'>
+                            <p>
+                                Te invitan este webinar, donde trataremos la temática de cómo humanizar 
+                                la experiencia del cliente de Banca y Seguros en el nuevo entorno digital.
+                            </p>
+                            <p>
+                                Además podremos conocer las estrategias que aplicó LOREM para generar una 
+                                experiencia memorable para sus clientes, mientras se convertía en el gran 
+                                Unicornio de LATAM.
+                            </p>
+                            <p>
+                                Escucha de primera mano la voz de nuestros especialistas:
+                            </p>
+                        </div>
 
-                <div>
-                    logos
-                </div>
+                        <div className='card-content'>
+                            <PersonalCard name={"José Perez"} jobTitle={"Presidente"}/>
+                            <PersonalCard name={"José Perez"} jobTitle={"Customer Service Senior Manager"}/>
+                            <PersonalCard name={"José Perez"} jobTitle={"Senior Customer Success Consultant"}/>
+                            <PersonalCard name={"José Perez"} jobTitle={"Gerente de Consultoría de Soluciones"}/>
+                        </div>
 
-                <div>
-                    <p>
-                        Participa e inspírate para innovar y mejorar la interacción entre clientes y marcas con historias de éxito de empresas del ámbito financiero en América Latina.
-                    </p>
-                    <p>
-                        ¡Te esperamos!
-                    </p>
-                </div>
+                        <div className='text-content'>
+                            <p>
+                                Participa e inspírate para innovar y mejorar la interacción entre clientes y marcas con historias de éxito de empresas del ámbito financiero en América Latina.
+                            </p>
+                            <p>
+                                ¡Te esperamos!
+                            </p>
+                        </div>
+                        <Brands/>
+                    </section>
 
-            </section>
-            <Brands/>
+                    <section className='rigth'>
+                        <div>
+                            <div>
+                                ¡Inscríbete y reserva tu lugar ahora!
+                            </div>
+                            <form action="">
+                                <div className='form-group'>
+                                    <label htmlFor="name"> nombre </label>
+                                    <input type="text" name="name" id="name" />
+                                </div>
+                                
+                            </form>
+                        </div>
+                    </section>
+
+                </div>
+            </div>
         </main>
     )
 }
