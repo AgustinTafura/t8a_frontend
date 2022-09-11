487316ff89f485ad5e6b19a1a8440e0344c40ea5
AgustinTafura <agustintafura@hotmail.com>
2022-09-11 17:19:24 -0300

    add loader component

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,13 @@
+import './style.scss'
+
+const Loader = () => {
+  return (
+    <div class="loader">
+    <span class="loader__element"></span>
+    <span class="loader__element"></span>
+    <span class="loader__element"></span>
+    </div>
+  )
+}
+
+export default Loader
