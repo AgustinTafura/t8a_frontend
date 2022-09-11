12235b41790a12d66091926fc095f73b87ff4910
AgustinTafura <agustintafura@hotmail.com>
2022-09-11 01:32:17 -0300

    footer updated

---
+++ src/containers/Footer.jsx
@@ -0,0 +1,51 @@
+const Footer = () => {
+  return (
+    <footer>
+        <div className="content container">
+            <div className="data">
+                ©  2020
+            </div>
+
+            <div className="social-icons">
+                
+                <a href="https://www.twitter.com.ar">
+                  <span className="icon">
+                  <i class="fa-brands fa-twitter fa-stack-1x"></i>
+                  </span>
+                </a>
+                
+                <a href="https://www.facebook.com.ar">
+                  <span className="icon">
+                      <i class="fab fa-facebook-f fa-stack-1x" aria-hidden="true"></i>
+                  </span>
+                </a>
+                
+                <a href="https://www.linkedin.com.ar">
+                  <span className="icon">
+                      <i class="fab fa-linkedin-in fa-stack-1x" aria-hidden="true"></i>
+                  </span>
+                </a>
+                <a href="https://www.youtube.com.ar">
+                  <span className="icon">
+                      <i class="fab fa-youtube fa-stack-1x" aria-hidden="true"></i>
+                  </span>
+                </a>
+                
+                <a href="https://www.instagram.com.ar">
+                  <span className="icon">
+                      <i class="fab fa-instagram fa-stack-1x" aria-hidden="true"></i>
+                  </span>
+                </a>
+                
+                <a href="https://www.snapchat.com.ar">
+                  <span className="icon">
+                      <i class="fab fa-snapchat fa-stack-1x" aria-hidden="true"></i>
+                  </span>
+                </a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
